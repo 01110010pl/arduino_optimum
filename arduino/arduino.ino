da9745e9613265bf40db688c663e999a93622de9
kambroz1 <01169000@pw.edu.pl>
2022-01-02 00:03:02 +0100

    Edycja arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,127 +1,200 @@
-// Biblioteki
-#include "DHT.h"
 #include <Wire.h>
 #include "MAX30100_PulseOximeter.h"
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
-#include <string>
-#include <iostream>
+#include "DHT.h"
+#include "SoftwareSerial.h"
+#include "Arduino.h"
+#include "DFRobotDFPlayerMini.h"
 
-// Derektywy preprocesora
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
+#define REPORTING_PERIOD_MS     1000
 #define DHTPIN 4
 #define DHTTYPE DHT11
-#define REPORTING_PERIOD_MS     1000
-#define OLED_RESET     -1
-#define SCREEN_ADDRESS 0x3C
 
-// Zmienne
-uint32_t tsLastReport = 0;
-int counter = 1;
+const int SZEROKOSC = 128;
+const int WYSOKOSC  = 64;
 
-// Obiekty modułów
-Adafruit_SSD1306 screen(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+SoftwareSerial mySoftwareSerial(1, 3); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
+
 DHT dht(DHTPIN, DHTTYPE);
+
+Adafruit_SSD1306 ekranik(SZEROKOSC, WYSOKOSC, &Wire, -1);
+
 PulseOximeter pox;
+
+uint32_t tsLastReport = 0;
+
+const int PIN14 = 14;
+const int PIN16 = 16;
+const int PIN25 = 25;
+const int PIN34 = 34;
+const int PIN5 = 5;
+const int PIN12 = 12;
+int status = 0;
+int poziom1 = 0;
+int poziom2 = 0;
+
+String listaTrybow [4] = {"1) Temperatura", "2) Muzyka", "3) Tryb pracy", "4) Pulsometr"};
 
 void onBeatDetected()
 {
     Serial.println("Beat!");
 }
 
-void setup() 
+void setup()
 {
-  Serial.begin(115200);
-  if(!screen.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) 
-  {
-    Serial.println(F("SSD1306 allocation failed"));
-    exit(0);
-  }
-  pox.setOnBeatDetectedCallback(onBeatDetected);
-  dht.begin();
-  pox.begin();
-  screen.setTextColor(WHITE);
-  screen.cp437(true);
+    Serial.begin(115200);
+    //Inicjalizacja ekranu
+    if(!ekranik.begin(SSD1306_SWITCHCAPVCC, 0x3C)) 
+    {
+        Serial.println("Błąd inicjalizacji wyświetlacza!");
+        exit(0);
+    }
+    ekranik.setTextColor(WHITE);
+    ekranik.cp437(true);
+
+    //Inicjalizacja pulsometru
+    if (!pox.begin()) {
+        Serial.println("FAILED");
+        for(;;);
+    } else 
+    {
+        Serial.println("SUCCESS");
+    }
+
+    //Inicjalizacja temperatury
+    dht.begin();
+    
+    // The default current for the IR LED is 50mA and it could be changed
+    //   by uncommenting the following line. Check MAX30100_Registers.h for all the
+    //   available options.
+    // pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+
+    // Register a callback for the beat detection
+    pox.setOnBeatDetectedCallback(onBeatDetected);
+
+    mySoftwareSerial.begin(9600);
+    if (!myDFPlayer.begin(mySoftwareSerial)) 
+    {  //Use softwareSerial to communicate with mp3.
+        Serial.println(F("Unable to begin:"));
+        Serial.println(F("1.Please recheck the connection!"));
+        Serial.println(F("2.Please insert the SD card!"));
+        while(true);
+    }
+    myDFPlayer.setTimeOut(500);
+    myDFPlayer.volume(10);
+    myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
+    myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
 }
 
-void temperature_humidity()
+String pulsometr()
 {
-   delay(2000);
-   float h = dht.readHumidity();
-   float t = dht.readTemperature();
-   float f = dht.readTemperature(true);
-   if (isnan(h) || isnan(t) || isnan(f)) 
-   {
-     Serial.println(F("Failed to read from DHT sensor!"));
-     return;
-   }
-   float hif = dht.computeHeatIndex(f, h);
-   float hic = dht.computeHeatIndex(t, h, false);
-   Serial.print(F("Humidity: "));
-   Serial.print(h);
-   Serial.print(F("%  Temperature: "));
-   Serial.print(t);
-   Serial.print(F("°C "));
-   Serial.print(f);
-   Serial.print(F("°F  Heat index: "));
-   Serial.print(hic);
-   Serial.print(F("°C "));
-   Serial.print(hif);
-   Serial.println(F("°F"));
+    pox.update();
+    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+        
+        Serial.print("Heart rate:");
+        Serial.print(pox.getHeartRate());
+        Serial.print("bpm / SpO2:");
+        Serial.print(pox.getSpO2());
+        Serial.println("%");
+        tsLastReport = millis();
+        return "Heart rate:" + String(pox.getHeartRate()) + "bpm\nSpO2:" + String(pox.getSpO2()) + "%";
+    }
 }
 
-String newTemperatureHumidity(int y)
+String temperatura()
 {
   delay(2000);
-   float h = dht.readHumidity();
-   float t = dht.readTemperature();
-   float f = dht.readTemperature(true);
-   if (isnan(h) || isnan(t) || isnan(f)) 
-   {
-     return F("Failed to read from DHT sensor!");
-   }
-   float hif = dht.computeHeatIndex(f, h);
-   float hic = dht.computeHeatIndex(t, h, false);
-   if(y == 1)
-   {
-      return "Humidity: " + String(h) + "%";
-   }
-   else if(y == 2)
-   {
-      return "Temperature: \n  " + String(t) + " C\n  " + String(f) + " F";
-   }
-   else if(y == 3)
-   {
-      return "Heat index: \n  " + String(hic) + " C\n  " + String(hif) + " F";
-   }
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return "";
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+  return "Temperature: " + String(t) + "°C \nHumidity: " + String(h) + "\nHeat index: " + String(hif);
 }
 
-void pulse()
+void muzyka()
 {
-  pox.update();
-  if (millis() - tsLastReport > REPORTING_PERIOD_MS) 
-  {
-       Serial.print("Heart rate:");
-       Serial.print(pox.getHeartRate());
-       Serial.print("bpm / SpO2:");
-       Serial.print(pox.getSpO2());
-       Serial.println("%");
-       tsLastReport = millis();
-   }
+   Serial.println("X");
 }
 
-void loop() 
+void pokazEkranu(String x)
 {
-      if(counter > 3)
-      {
-        counter = 1;
-      }
-      screen.clearDisplay();
-      screen.setTextSize(1);
-      screen.setCursor(10, 0);
-      screen.print(newTemperatureHumidity(counter));
-      screen.display();
-      counter++;
-      delay(250);
+    ekranik.setTextSize(1);
+    ekranik.setCursor(10, 0);
+    ekranik.print(x);
+    ekranik.display();
+    delay(250);
+}
+
+void loop()
+{
+    if(digitalRead(PIN25) == LOW)
+    {
+        poziom2 += 1;
+        if(poziom2 == 4)
+        {
+            poziom2 = 0;
+        }
+    }
+    if(digitalRead(PIN14) == LOW)
+    {
+        if(poziom1 == 0)
+        {
+            poziom1 += 1;
+        }
+    }
+    if(poziom1 == 0)
+    {
+        pokazEkranu(listaTrybow[poziom2]);
+    }
+    else
+    {
+        switch (poziom2)
+        {
+            case 1:
+            {
+                pokazEkranu(temperatura());
+                break;
+            }
+            case 2:
+            {
+                muzyka();
+                break;
+            }
+            case 3:
+            {
+                pokazEkranu(pulsometr());
+                break;
+            }
+        }
+    }
 }
