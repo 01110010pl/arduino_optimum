2f9e3491be028bb3978f410791a93f629051bcf3
kambroz1 <01169000@pw.edu.pl>
2022-01-09 11:04:06 +0100

    Dodanie menu i podstawy kodu do arduino.ino; bez muzyki

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,97 +2,224 @@
 #include "DHT.h"
 #include <Wire.h>
 #include "MAX30100_PulseOximeter.h"
-#include "Arduino.h"
-#include "SoftwareSerial.h"
-#include "DFRobotDFPlayerMini.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 // Derektywy preprocesora
-#define DHTPIN 4
-#define DHTTYPE DHT11
-#define REPORTING_PERIOD_MS     1000
+#define DHTPIN  4
+#define DHTTYPE DHT11   // DHT 11
+#define REPORTING_PERIOD_MS 1000
 
+// Klasy
+class Temperature 
+{
+  public:
+    float temC;
+    float temF;
+    float humidity;
+    float indHC;
+    float indHF;
+    Temperature()
+    {
+      temC = 0;
+      temF = 0;
+      humidity = 0;
+      indHC = 0;
+      indHF = 0;
+    }
+};
+
+class Pulse
+{
+  public:
+    float heartRate;
+    float sp02;
+};
+
+class Light
+{
+  public:
+    bool turnOn;
+    Light()
+    {
+      turnOn = false;
+    }
+};
+
+class MP3
+{
+  public:
+    bool turnOn;
+    MP3()
+    {
+      turnOn = false;
+    }
+};
+
+class Menu
+{
+  public:
+    int side1;
+    int side2;
+  Menu()
+  {
+    side1 = 1;
+    side2 = 1;
+  }
+};;
+
+// Zmienne
+uint32_t tsLastReport = 0;
+const int SZEROKOSC = 128;
+const int WYSOKOSC  = 64;
+const int BUTTON_NEXT = 39;
+const int BUTTON_ACCEPT = 36;
+const int BUTTON_EXIT = 34;
+const int LED_RED = 18;
+const int LED_GREEN= 19;
+const int LED_BLUE = 23;
+Temperature temp;
+Menu menu;
+MP3 mp3;
+Light light;
+Pulse pulse;
+
+// Inicjalizacja czujników
 DHT dht(DHTPIN, DHTTYPE);
 PulseOximeter pox;
-SoftwareSerial mySoftwareSerial(1, 3); // RX, TX
-DFRobotDFPlayerMini myDFPlayer;
+Adafruit_SSD1306 ekran(SZEROKOSC, WYSOKOSC, &Wire, -1);
 
-void printDetail(uint8_t type, int value);
-
-uint32_t tsLastReport = 0;
-
-void setup()
+// Setup
+void setup() 
 {
-    Serial.begin(115200);
-    
-    // MP3
-    mySoftwareSerial.begin(9600);
-    if (!myDFPlayer.begin(mySoftwareSerial)) 
+  Serial.begin(115200);
+
+  // PINY
+  pinMode(BUTTON_ACCEPT, INPUT_PULLUP);
+  pinMode(BUTTON_EXIT, INPUT_PULLUP);
+  pinMode(BUTTON_NEXT, INPUT_PULLUP);
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+
+  // Puls
+  if (!pox.begin()) 
+  {
+    Serial.println("FAILED");
+    for(;;);
+  } 
+  else Serial.println("SUCCESS");
+  pox.setOnBeatDetectedCallback(onBeatDetected);
+
+  // Temperatura
+  dht.begin();
+
+  // Ekran
+  if(!ekran.begin(SSD1306_SWITCHCAPVCC, 0x3C)) 
+  {
+    Serial.println("Błąd inicjalizacji wyświetlacza!");
+    exit(0); // Nie idź dalej.
+  }
+  ekran.setTextColor(WHITE);
+  ekran.cp437(true);
+}
+
+// Metody
+void temperatura()
+{
+  delay(2000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+  if (isnan(h) || isnan(t) || isnan(f)) 
+  {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  float hif = dht.computeHeatIndex(f, h);
+  float hic = dht.computeHeatIndex(t, h, false);
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+}
+
+void onBeatDetected()
+{
+    Serial.println("WYkryto puls!");
+}
+
+void puls()
+{
+  pox.update();
+  if (millis() - tsLastReport > REPORTING_PERIOD_MS) 
+  {
+      Serial.print("Heart rate:");
+      Serial.print(pox.getHeartRate());
+      Serial.print("bpm / SpO2:");
+      Serial.print(pox.getSpO2());
+      Serial.println("%");
+      tsLastReport = millis();
+  }
+}
+
+void showLCD(String message)
+{
+  ekran.clearDisplay();
+  ekran.setTextSize(1.5);
+  ekran.setCursor(10, 0);
+  ekran.print(message);
+  ekran.display();
+  delay(250);
+}
+
+// Loop
+void loop() 
+{
+  if(digitalRead(BUTTON_NEXT) == HIGH)
+  {
+    if(menu.side2 == 1)
     {
-        Serial.println(F("Unable to begin:"));
-        Serial.println(F("1.Please recheck the connection!"));
-        Serial.println(F("2.Please insert the SD card!"));
-        while(true);
+      if(menu.side1 == 5) menu.side1 = 1;
+      else menu.side1 += 1;
     }
-    myDFPlayer.setTimeOut(500);
-    myDFPlayer.volume(15);
-    myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
-    myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
-
-    // Temperatura
-    dht.begin();
-
-    // Pulsometr
-    if (!pox.begin())
+  }
+  if(menu.side2 == 1)
+  {
+    switch(menu.side1)
     {
-        Serial.println("FAILED");
-        for(;;);
-    } 
-    else Serial.println("SUCCESS");
-}
-
-String temperatura()
-{
-    delay(2000);
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
-    float f = dht.readTemperature(true);
-    if (isnan(h) || isnan(t) || isnan(f)) 
-    {
-        Serial.println(F("Failed to read from DHT sensor!"));
-        return;
+      case 1:
+      {
+        showLCD("1) Puls");
+        break;
+      }
+      case 2:
+      {
+        showLCD("2) Temperatura");
+        break;
+      }
+      case 3:
+      {
+        showLCD("3) Swiatlo");
+        break;
+      }
+      case 4:
+      {
+        showLCD("4) Muzyka");
+        break;
+      }
+      case 5:
+      {
+        showLCD("5) Tryb pracy");
+        break;
+      }
     }
-    float hif = dht.computeHeatIndex(f, h);
-    float hic = dht.computeHeatIndex(t, h, false);
-    String message = "Temperatura: " + String(t) + "°C";
-    x += "\n" + "Wilgotność: " String(h) + "%";
-    x += "\n" + "Indeks ciepła: " + String(hic) + "°C/" + String(hif) + "°F";
-    return message;
-}
-
-String pulsometr()
-{
-    pox.update();
-    if (millis() - tsLastReport > REPORTING_PERIOD_MS) 
-    {
-        Serial.print("Heart rate:");
-        Serial.print();
-        Serial.print("bpm / SpO2:");
-        Serial.print();
-        Serial.println("%");
-        tsLastReport = millis();
-        Strign message = "";
-        message += "Puls: " + String(pox.getHeartRate()) + "bpm";
-        message += "\n" + "Sp02: " + String(pox.getSpO2());
-        return message;
-    }
-}
-
-void muzyka()
-{
-    myDFPlayer.randomAll();
-}
-
-void loop()
-{
-
+  }
 }
